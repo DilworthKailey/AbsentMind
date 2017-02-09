cb3cf6f67380f438d718a698f8fbe4041b24238f
Kailey <dil11004@byui.edu>
2017-02-09 09:25:54 -0700

    2/9/2017 - finished setPassword and passwordControlTest

--- AbsentMind/src/Control/PasswordControl.java
+++ AbsentMind/src/Control/PasswordControl.java
@@ -28,74 +28,80 @@ public class PasswordControl {
         third_letter = Character.toUpperCase(third_letter);
         fourth_letter = Character.toUpperCase(fourth_letter);
         
-        if (first_letter == 'A' | first_letter == 'B' | first_letter == 'C' | first_letter == 'D' | first_letter == 'E' | first_letter == 'F'| first_letter == 'G'){
-            value1 = 1;
-        }
-        if (first_letter == 'H' | first_letter == 'I' | first_letter == 'J' | first_letter == 'K' | first_letter == 'L' | first_letter == 'M'){
+        if (first_letter == 65 | first_letter == 66 | first_letter == 67 | first_letter == 68 | first_letter == 69 | first_letter == 70 | first_letter == 71){
+            value1 = 1; }
+        else if (first_letter == 72 | first_letter == 73 | first_letter == 74 | first_letter == 75 | first_letter == 76 | first_letter == 77 ){
             value1 = 2;
         }
-        if (first_letter == 'N' | first_letter == 'O' | first_letter == 'P' | first_letter == 'Q' | first_letter == 'R' | first_letter == 'S'){
+        else if (first_letter == 78 | first_letter == 79 | first_letter == 80 | first_letter == 81 | first_letter == 82 | first_letter == 83 ){
             value1 = 3;
         }
-        if (first_letter == 'T' | first_letter == 'U' | first_letter == 'V' | first_letter == 'W' | first_letter == 'X' | first_letter == 'Y'){
+        else if (first_letter == 84 | first_letter == 85 | first_letter == 86 | first_letter == 87 | first_letter == 88 | first_letter == 89){
             value1 = 4;
         }
         else{
-            value1 = 5;
-        }
-        
-        if (second_letter == 'A' | first_letter == 'B' | first_letter == 'C' | first_letter == 'D' | first_letter == 'E' | first_letter == 'F'| first_letter == 'G'){
+            value1 = 5;   
+        }    
+
+         if (second_letter == 65 | second_letter == 66 | second_letter == 67 | second_letter == 68 | second_letter == 69 | second_letter == 70 | second_letter == 71){
             value2 = 1;
-        }
-        if (second_letter == 'H' | first_letter == 'I' | first_letter == 'J' | first_letter == 'K' | first_letter == 'L' | first_letter == 'M'){
+         }
+         else if (second_letter == 72 | second_letter == 73 | second_letter == 74 | second_letter == 75 | second_letter == 76 | second_letter == 77 ){
             value2 = 2;
-        }
-        if (second_letter == 'N' | first_letter == 'O' | first_letter == 'P' | first_letter == 'Q' | first_letter == 'R' | first_letter == 'S'){
+         }
+         else if (second_letter == 78 | second_letter == 79 | second_letter == 80 | second_letter == 81 | second_letter == 82 | second_letter == 83 ){
             value2 = 3;
-        }
-        if (second_letter == 'T' | first_letter == 'U' | first_letter == 'V' | first_letter == 'W' | first_letter == 'X' | first_letter == 'Y'){
+         }
+         else if (second_letter ==  84 | second_letter == 85 | second_letter == 86 | second_letter == 87 | second_letter == 88 | second_letter == 89){
             value2 = 4;
-        }
+         }
         else{
-            value2 = 5;
-        }
+            value2 = 5;}
         
-        if (third_letter == 'A' | first_letter == 'B' | first_letter == 'C' | first_letter == 'D' | first_letter == 'E' | first_letter == 'F'| first_letter == 'G'){
+        if (third_letter == 65 | third_letter == 66 | third_letter == 67 | third_letter == 68 | third_letter == 69 | third_letter == 70 | third_letter == 71){
             value3 = 1;
         }
-        if (third_letter == 'H' | first_letter == 'I' | first_letter == 'J' | first_letter == 'K' | first_letter == 'L' | first_letter == 'M'){
+        else if (third_letter == 72 | third_letter == 73 | third_letter == 74 | third_letter == 75 | third_letter == 76 | third_letter == 77){
             value3 = 2;
         }
-        if (third_letter == 'N' | first_letter == 'O' | first_letter == 'P' | first_letter == 'Q' | first_letter == 'R' | first_letter == 'S'){
+        else if (third_letter == 78 | third_letter == 79 | third_letter == 80 | third_letter == 81 | third_letter == 82 | third_letter == 83){
             value3 = 3;
         }
-        if (third_letter == 'T' | first_letter == 'U' | first_letter == 'V' | first_letter == 'W' | first_letter == 'X' | first_letter == 'Y'){
+        else if (third_letter == 84 | third_letter == 85 | third_letter == 86 | third_letter == 87 | third_letter == 88 | third_letter == 89){
             value3 = 4;
         }
         else{
             value3 = 5;
         }
         
-        if (fourth_letter == 'A' | first_letter == 'B' | first_letter == 'C' | first_letter == 'D' | first_letter == 'E' | first_letter == 'F' | first_letter == 'G'){
+        if (fourth_letter == 65 | fourth_letter == 66 | fourth_letter == 67 | fourth_letter == 68 | fourth_letter == 69 | fourth_letter == 70 | fourth_letter == 71){
             value4 = 1;
         }
-        if (fourth_letter == 'H' | first_letter == 'I' | first_letter == 'J' | first_letter == 'K' | first_letter == 'L' | first_letter == 'M'){
+        else if (fourth_letter == 72 | fourth_letter == 73 | fourth_letter == 74 | fourth_letter == 75 | fourth_letter == 76 | fourth_letter == 77){
             value4 = 2;
         }
-        if (fourth_letter == 'N' | first_letter == 'O' | first_letter == 'P' | first_letter == 'Q' | first_letter == 'R' | first_letter == 'S'){
+        else if (fourth_letter == 78 | fourth_letter == 79 | fourth_letter == 80 | fourth_letter == 81 | fourth_letter == 82 | fourth_letter == 83){
             value4 = 3;
         }
-        if (fourth_letter == 'T' | first_letter == 'U' | first_letter == 'V' | first_letter == 'W' | first_letter == 'X' | first_letter == 'Y'){
+        else if (fourth_letter == 84 | fourth_letter == 85 | fourth_letter == 86 | fourth_letter == 87 | fourth_letter == 88 | fourth_letter == 89){
             value4 = 4;
         }
         else{
             value4 = 5;
         }
         
-        value5 = Math.sqrt(Math.pow(value1 + value2, 2) + Math.pow(value3 + value4, 2)) * 10000;
+            value5 = Math.sqrt(Math.pow((value1 + value2), 2) + Math.pow((value3 + value4), 2)) * 10000;
         
+     
         double password = Math.round(value5);
         
+        if (password < 100000){
+           password = password + 100000;
+        }
+        if (password > 999999){
+           password = -1;
+           System.out.println("Input must be less than 1000000.");
+        }
         return password;
     }
     
---
+++ AbsentMind/test/Control/PasswordControlTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Josh
+ */
+public class PasswordControlTest {
+    
+    public PasswordControlTest() {
+    }
+
+    /**
+     * Test of setPassword method, of class PasswordControl.
+     */
+    @Test
+    public void testSetPassword() {
+        System.out.println("setPassword");
+        
+        System.out.println("\tTest case #1");
+        String playerName = "freddy";
+        PasswordControl instance = new PasswordControl();
+        double expResult = 144721.0;
+        double result = instance.setPassword(playerName);
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        System.out.println(result);
+        
+        System.out.println("\tTest case #2");
+        playerName = "zzzz";
+       
+        expResult = 141421.0;
+        result = instance.setPassword(playerName);
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        System.out.println(result);
+        
+        System.out.println("\tTest case #3");
+        playerName = "zaj5";
+       
+        expResult = 192195.0;
+        result = instance.setPassword(playerName);
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        System.out.println(result);
+        
+        System.out.println("\tTest case #4");
+        playerName = "1235";
+       
+        expResult = 141421.0;
+        result = instance.setPassword(playerName);
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        System.out.println(result);
+        
+        System.out.println("\tTest case #5");
+        playerName = "!@#$";
+       
+        expResult = 141421.0;
+        result = instance.setPassword(playerName);
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        System.out.println(result);
+        
+        System.out.println("\tTest case #5");
+        playerName = "2a55";
+       
+        expResult = 116619.0;
+        result = instance.setPassword(playerName);
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        System.out.println(result);
+    }
+    
+}
